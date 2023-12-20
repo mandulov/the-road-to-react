2189f2e00965882df6cdb01ab4c4859a40986972
Kostadin Mandulov <mandulov@gmail.com>
2023-12-20 20:06:56 +0200

    after "Meet another React Component"

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { FC, ReactNode } from "react";
+import { FC, Fragment, ReactNode } from "react";
 
 const list = [
   {
@@ -21,35 +21,49 @@ const list = [
 
 type ListItem = (typeof list)[number];
 
+const List: FC = () => {
+  return (
+    <ul>
+      {/* if array items do not have an unique value, */}
+      {/* their indices could be used as a last resort, */}
+      {/* given that the order of the items in the array doesn't change */}
+      {list.map(
+        (x: ListItem): ReactNode => (
+          <li key={x.objectID}>
+            <span>
+              <a href={x.url} target="_blank">
+                {x.title}
+              </a>
+            </span>
+            <span>{x.author}</span>
+            <span>{x.num_comments}</span>
+            <span>{x.points}</span>
+          </li>
+        )
+      )}
+    </ul>
+  );
+};
+
+const Search: FC = () => {
+  return (
+    <Fragment>
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+    </Fragment>
+  );
+};
+
 const App: FC = () => {
   return (
     <div>
       <h1>My Hacker Stories</h1>
 
-      <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <Search />
 
       <hr />
 
-      <ul>
-        {/* if array items do not have an unique value, */}
-        {/* their indices could be used as a last resort, */}
-        {/* given that the order of the items in the array doesn't change */}
-        {list.map(
-          (x: ListItem): ReactNode => (
-            <li key={x.objectID}>
-              <span>
-                <a href={x.url} target="_blank">
-                  {x.title}
-                </a>
-              </span>
-              <span>{x.author}</span>
-              <span>{x.num_comments}</span>
-              <span>{x.points}</span>
-            </li>
-          )
-        )}
-      </ul>
+      <List />
     </div>
   );
 };
