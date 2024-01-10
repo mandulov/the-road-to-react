f205a4070b894a43480630786183d99d81ae5c3e
Kostadin Mandulov <mandulov@gmail.com>
2024-01-10 21:04:10 +0100

    after "Third-Party Libraries in React"

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import axios, { AxiosResponse } from "axios";
 import {
   ChangeEvent,
   Dispatch,
@@ -117,8 +118,8 @@ const STORIES_PROP_NAME = "hits";
 const isValidResponseBody = (body: unknown): body is { [STORIES_PROP_NAME]: Story[] } => isObject(body) && STORIES_PROP_NAME in body;
 
 const HN_API_BASE_URL = "https://hn.algolia.com"
-const fetchStories = (query: string): Promise<Story[]> => fetch(`${HN_API_BASE_URL}/api/v1/search?query=${query}`)
-  .then((res: Response): unknown => res.json())
+const fetchStories = (query: string): Promise<Story[]> => axios.get(`${HN_API_BASE_URL}/api/v1/search?query=${query}`)
+  .then((res: AxiosResponse<unknown>): unknown => res.data)
   .then((body: unknown): Story[] => {
     if (isValidResponseBody(body)) {
       return body.hits;
